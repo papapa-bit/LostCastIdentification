8c9443324459dcadf58242195bf26e40db76f054
papillon <m.aalif0314@gmail.com>
2025-06-21 00:47:16 +0800

    Deteled and archived unused files

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py 
-from feature_extractor import extract_features
-
-img_path = 'dataset/bento-front.jpg'
-vector = extract_features(img_path)
-
-print("Feature vector length:", len(vector))
-print("Feature preview:", vector[:10])
